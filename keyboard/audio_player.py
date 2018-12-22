2b23b3b0ee484ca3bf79111df458b86dc2896305
ke_ix1 <xkei4dev@gmail.com>
2018-12-22 11:50:18 +0900

    cry myao

--- keyboard/audio_player.py
+++ keyboard/audio_player.py
@@ -4,13 +4,17 @@ import AppKit
 import time
 from pydub import AudioSegment
 from pydub.playback import play
+from pydub.utils import ratio_to_db
 import threading
 
 class AudioPlayer:
     def __init__(self, filename):
-        self.filepath="../data/bubble/"+str(filename)+".wav"
+        # self.filepath="../data/bubble/"+str(filename)+".wav"
+        self.filepath="../data/cat/"+str(filename)+".mp3"
         # self.filepath="../data/bubble/1.wav"
-        self.sound = AudioSegment.from_file(self.filepath, "wav")
+        # self.filepath="../data/cat/cat.mp3"
+        self.sound = AudioSegment.from_file(self.filepath, "mp3")
+        self.sound = self.sound + ratio_to_db(0.3)
     def play(self):
         play(self.sound)
 
